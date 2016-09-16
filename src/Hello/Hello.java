f5e4a99084fb3889110ddbdef917aecab1c078ef
Reegly <reeglylorean@gmail.com>
2016-09-17 00:32:47 +0700

    Commons cli
    Added library to the project.

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -1,8 +1,7 @@
 package Hello;
 
 public class Hello {
-public static void main(String [] args)
-{
-    System.out.println("Hello World !");
-}
+    public static void main(String [] args) {
+        System.out.println("Hello World !");
+    }
 }
