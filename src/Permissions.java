4f6452ba55a1070588bdce7aa53c421b0f8c0da5
Stig4All <Stan422@gmail.com>
2016-10-01 11:26:04 +0700

    Add permission class

--- src/Permissions.java
+++ src/Permissions.java
@@ -2,5 +2,13 @@
  * Created by reegl on 29.09.2016.
  */
 public class Permissions {
+    private String permname;
+    private int digit;
 
+    public void Permissions(String s,int d)
+    {
+        pername=s;
+        digit=d;
+    }
 }
+
