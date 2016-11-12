19f037c1709de9ff28e3e1a2a9e70a0f57c88e47
Reegly <reeglylorean@gmail.com>
2016-11-12 11:43:13 +0700

    (F) Minor fixes in Hash.java

--- src/main/java/Hash.java
+++ src/main/java/Hash.java
@@ -15,20 +15,15 @@ public class Hash {
         MessageDigest md5;
         StringBuffer hexString = new StringBuffer();
 
-        try {
-            md5 = MessageDigest.getInstance("md5");
-            md5.reset();
-            md5.update(str.getBytes());
+        md5 = MessageDigest.getInstance("md5");
+        md5.reset();
+        md5.update(str.getBytes());
 
-            byte messageDigest[] = md5.digest();
+        byte messageDigest[] = md5.digest();
 
-            for (int i = 0; i < messageDigest.length; i++) {
-                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+        for (int i = 0; i < messageDigest.length; i++) {
+            hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
             }
-
-        } catch (NoSuchAlgorithmException e) {
-            return e.toString();
-        }
 
         return hexString.toString();
     }
